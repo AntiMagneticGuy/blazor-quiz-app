c0287202e35646b90cf5af5b717ee5c891b81e2a
Filip <35558467+AntiMagneticGuy@users.noreply.github.com>
2023-02-22 17:41:20 +0100

    first

---
+++ quiz app opgave/Shared/Class.cs
@@ -0,0 +1,31 @@
+﻿namespace quiz_app_opgave.Shared
+{
+    public class Spørgsmål // json
+    {
+        public string category { get; set; }
+
+        public string type { get; set; }
+
+        public string difficulty { get; set; }
+
+        public string question { get; set; }
+
+        public string correct_answer { get; set; }
+
+        public string[] incorrect_answers { get; set; }
+
+    }
+
+   
+    public static class Svar
+    {
+        public static string[,] svarMuligheder { get; set; } = new string[4,4];
+
+        public static string[] userAnswer { get; set; } = new string[4];
+        public static string[] question { get; set; } = new string[4];
+
+
+        public static string[] correctAnswer { get; set; } = new string[4];
+    }
+
+}
